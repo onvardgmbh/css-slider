d1d1fff5da7cb9891df8d39fb94513effbb784fc
Björn Hasse <hasse@onvard.de>
2016-07-27 14:32:25 +0200

    slider.js no mousedown + no swipe and scroll

--- js/slider.js
+++ js/slider.js
@@ -9,12 +9,18 @@
 		var radios = Array.prototype.slice.call(slider.querySelectorAll('input'));
 		var active;
 		var touchStartX;
+		var touchStartY;
 		var deltaX;
+		var deltaY;
+		var scrolling;
 
 		function onTouchstart(event) {
 			var touch = event.touches ? event.touches[0] : event;
 			touchStartX = touch.pageX;
+			touchStartY = touch.pageY;
 			deltaX = 0;
+			deltaY = 0;
+			scrolling = undefined;
 
 			active = radios
 				.map(function(el) {
@@ -25,23 +31,28 @@
 			// Add event listeners needed during drag
 			slider.addEventListener('touchmove', onTouchmove);
 			slider.addEventListener('touchend', onTouchend);
-			slider.addEventListener('mousemove', onTouchmove);
-			slider.addEventListener('mouseleave', onTouchend);
-			slider.addEventListener('mouseup', onTouchend);
 		}
 
 		function onTouchmove(event) {
 			var touch = event.touches ? event.touches[0] : event;
-			deltaX =  touch.pageX - touchStartX;
+			deltaX = touch.pageX - touchStartX;
+			deltaY = touch.pageY - touchStartY;
 
-			firstSlide.style.transition = 'none'; // Disable transitions during dragging
-			firstSlide.style.marginLeft = deltaX - firstSlide.offsetWidth * active + 'px';
+			if (typeof scrolling === 'undefined') {
+				scrolling = Math.abs(deltaX) < Math.abs(deltaY);
+			}
+
+			if (!scrolling) {
+				event.preventDefault();
+				firstSlide.style.transition = 'none'; // Disable transitions during dragging
+				firstSlide.style.marginLeft = deltaX - firstSlide.offsetWidth * active + 'px';
+			}
 		}
 
 		function onTouchend(event) {
 			var perPage = Math.round(firstSlide.parentNode.clientWidth / firstSlide.offsetWidth);
 
-			if (Math.abs(deltaX) > firstSlide.offsetWidth / 4) {
+			if (!scrolling && Math.abs(deltaX) > firstSlide.offsetWidth / 2) {
 				var next = Math.max(0, Math.min(radios.length - perPage, deltaX > 0
 					? active - (active % perPage || perPage) // Next page to the left
 					: active - active % perPage + perPage // Next page to the right
@@ -55,13 +66,9 @@
 			// Remove event listeners that are only needed during drag
 			slider.removeEventListener('touchmove', onTouchmove);
 			slider.removeEventListener('touchend', onTouchend);
-			slider.removeEventListener('mousemove', onTouchmove);
-			slider.removeEventListener('mouseleave', onTouchend);
-			slider.removeEventListener('mouseup', onTouchend);
 		}
 
 		slider.addEventListener('touchstart', onTouchstart);
-		slider.addEventListener('mousedown', onTouchstart);
 	}
 
 	/**
@@ -69,7 +76,7 @@
 	 */
 	document.addEventListener('DOMContentLoaded', function() {
 		Array.prototype.forEach.call(
-			document.querySelectorAll('.css-slider'),
+			document.querySelectorAll('.csslider'),
 			setupSlider
 		);
 	});
